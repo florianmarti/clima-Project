2e1a0e1b4f6f86e46c35a89ba230e4d37f935b23
Florian Hernan Marti <florianmarti@gmail.com>
2025-08-02 17:59:18 -0600

    feat: install Material UI and add a basic header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
- 
+// src/App.jsx
 
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 
 function App() {
-   
-
   return (
-    <h1>Hola mundo!</h1>
-  )
+    <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <WbSunnyIcon fontSize="large" color="primary" />
+      <Typography variant="h3" component="h1" gutterBottom>
+        Aplicación del Clima
+      </Typography>
+      <Typography variant="body1">
+        ¡Tu proyecto de portafolio está tomando forma!
+      </Typography>
+    </Container>
+  );
 }
 
-export default App
+export default App;
