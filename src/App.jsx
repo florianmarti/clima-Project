192f1b5c120eb63bb445963acc3605b73dfd24aa
Florian Hernan Marti <florianmarti@gmail.com>
2025-08-02 18:05:31 -0600

    refactor: move header code into a new Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 // src/App.jsx
 
-import React from 'react';
-import { Container, Typography } from '@mui/material';
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: '2rem' }}>
-      <WbSunnyIcon fontSize="large" color="primary" />
-      <Typography variant="h3" component="h1" gutterBottom>
-        Aplicación del Clima
-      </Typography>
-      <Typography variant="body1">
-        ¡Tu proyecto de portafolio está tomando forma!
-      </Typography>
-    </Container>
+     <Header />
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+// src/components/Header.jsx
+
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+
+function Header() {
+  return (
+    <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <WbSunnyIcon fontSize="large" color="primary" />
+      <Typography variant="h3" component="h1" gutterBottom>
+        Aplicación del Clima
+      </Typography>
+      <Typography variant="body1">
+        ¡Tu proyecto de portafolio está tomando forma!
+      </Typography>
+    </Container>
+  );
+}
+
+export default Header;
